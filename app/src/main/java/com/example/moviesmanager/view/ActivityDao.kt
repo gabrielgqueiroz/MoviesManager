ed70e84edbd114be1bf0a8ba7dc9a453f0eb0195
gabrielgqueiroz <ggabriel12.gg@gmail.com>
2022-12-05 23:04:28 -0300

    deleção de uma classe de teste

--- app/src/main/java/com/example/moviesmanager/view/ActivityDao.kt
+++
@@ -1,8 +0,0 @@
-package com.example.moviesmanager.view
-
-import androidx.appcompat.app.AppCompatActivity
-import com.example.moviesmanager.model.entity.Movie
-
-abstract class ActivityDao: AppCompatActivity() {
-    abstract fun updateMovieList(_movieList: MutableList<Movie>)
-}
